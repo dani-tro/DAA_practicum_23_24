a1055d72fe21166f0da6b53c5890134d3d7463d9
dani-tro <danitropolinov@gmail.com>
2024-05-20 19:13:16 +0300

    Week 11 added

---
+++ Week-12/knapsack.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+#define MAX 1000
+
+int N, M, weight[MAX], value[MAX], F[MAX][MAX];
+
+void fill_F()
+{
+	for(int j = 0; j <= M; j++) F[0][j] = 0;
+	for(int i = 1; i <= N; i++) F[i][0] = 0;
+	for(int i = 1; i <= N; i++)
+	{
+		for(int j = 1; j <= M; j++)
+		{
+			if(weight[i] <= j) 
+				F[i][j] = max(F[i - 1][j], 
+							  F[i - 1][j - weight[i]] + value[i]);
+			else F[i][j] = F[i - 1][j];
+		}
+	}
+}
+
+void find_solution()
+{
+	int j = M;
+	for(int i = N; i >= 1; i--)
+	{
+		if(F[i][j] != F[i - 1][j])
+		{
+			cout << i << " ";
+			j = j - weight[i];
+		}
+	}
+	cout << endl;
+}
+
+int main()
+{
+	cin >> N >> M;
+	for(int i = 1; i <= N; i++) cin >> weight[i];
+	for(int i = 1; i <= N; i++) cin >> value[i];
+	fill_F();
+	cout << "Максималната печалба е : " << F[N][M] << endl;
+	cout << "Трябва да вземем предмети с номера : ";
+	find_solution();
+	return 0;
+}
+
